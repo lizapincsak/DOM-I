e604aa65a01669ceec3e8d0ab0018cb08a85f218
Liza Pincsak <lizapincsak@Lizas-MacBook.local>
2020-11-30 16:17:30 -0600

    finished mvp

--- js/index.js
+++ js/index.js
@@ -40,3 +40,76 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//navBar
+const servicesLink = document.createElement('a');
+servicesLink.textContent = "Services";
+document.querySelector('nav').prepend(servicesLink);
+
+
+const productLink = servicesLink.nextElementSibling;
+productLink.textContent = "Product";
+
+const visionLink = productLink.nextElementSibling;
+visionLink.textContent = "Vision";
+
+const featuresLink = visionLink.nextElementSibling;
+featuresLink.textContent = "Features";
+
+const aboutLink = featuresLink.nextElementSibling;
+aboutLink.textContent = "About";
+
+const contactLink = aboutLink.nextElementSibling;
+contactLink.textContent = "Contact";
+
+//cta section
+const mainTitle = document.querySelector('h1');
+
+mainTitle.innerHTML = "DOM <br /> Is <br /> Awesome" ;
+
+const button = document.querySelector('button');
+button.textContent = "Get Started";
+
+const imageCircle = document.getElementById('cta-img');
+console.log(imageCircle);
+imageCircle.src = '../img/header-img.png';
+
+//main-content 
+const mainContent = document.querySelectorAll(".text-content");
+mainContent[0].querySelector("h4").textContent = "Features";
+mainContent[0].querySelector("p").textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis.";
+
+mainContent[1].querySelector("h4").textContent = "About";
+mainContent[1].querySelector("p").textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+mainContent[2].querySelector("h4").textContent = "Services";
+mainContent[2].querySelector("p").textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio,in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis.";
+
+mainContent[3].querySelector("h4").textContent = "Product";
+mainContent[3].querySelector("p").textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, iinterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metusscelerisque quis.";
+mainContent[4].querySelector("h4").textContent = "Vision";
+mainContent[4].querySelector("p").textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, ininterdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+//main-content image
+const maincontentImg = document.getElementById('middle-img');
+maincontentImg.src = '../img/mid-page-accent.jpg';
+
+//contact
+
+const contactTitle = document.querySelector('.contact h4');
+contactTitle.textContent = "Contact";
+console.log(contactTitle)
+
+const address = contactTitle.nextElementSibling;
+address.innerHTML = "123 Way 456 Street <br /> Somewhere, USA";
+
+const phoneNumber = address.nextElementSibling;
+phoneNumber.textContent = "1 (888) 888-8888";
+
+const email = phoneNumber.nextElementSibling;
+email.textContent = "sales@greatidea.io";
+
+//footer
+
+const footer = document.querySelector('footer');
+footer.textContent = "Copyright Great Idea! 2018"
